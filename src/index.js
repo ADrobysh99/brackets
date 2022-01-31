9a3f368b4f47112c90ed2b4cd57d0874e3745935
Anastasiya <anastasiyadrobysh99@gmail.com>
2022-01-31 22:59:23 +0300

    feat: add solution btackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+   module.exports = function check(str, bracketsConfig) {
+    const Map = {};
+    const Array = [];
+    bracketsConfig.forEach(config => {
+      Map[config[0]] = config[1];
+    });
+    for (let i = 0; i < str.length; i++) {
+      if (Map[Array[Array.length - 1]] === str[i]) {
+        Array.pop();
+      } else {
+        Array.push(str[i]);
+      }
+    }
+    return Array.length === 0;
+  }
